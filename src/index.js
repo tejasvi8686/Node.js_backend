659e1fc3da1da580b7ffe05c0f9ab8e7f6ce6ffa
tejavi8686 <tejasviraj8686@gmail.com>
2024-08-20 15:43:50 +0530

    make db connection'

---
+++ src/index.js
@@ -0,0 +1,39 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "./constant";
+
+
+
+/*
+
+
+
+
+import express from "express";
+const app = express()
+(async () => {
+  try {
+    await mongoose.connect(`${Process.env.MONGO_URI}/${DB_NAME}`);
+    app.on("error", (error) => {
+      console.log("ERROR", error);
+      throw error;
+    });
+
+    app.listen(process.env.PORT, () => {
+        console.log(`APP is listinig on Port $
+            {process.env.PORT}`);
+    })
+
+
+  } catch (error) {
+    console.error("ERROR: ", error);
+    throw err;
+  }
+})();
+
+
+*/
+
+
+
+
+
