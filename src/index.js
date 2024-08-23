e87de7e4cc3b872761372ebf454b29a835a7537a
tejavi8686 <tejasviraj8686@gmail.com>
2024-08-23 19:41:41 +0530

    async handler

--- src/index.js
+++ src/index.js
@@ -1,21 +1,24 @@
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 import connectDB from "./db/index.js";
 
-
-
 dotenv.config({
-    path: './env'
-})
+  path: "./env",
+});
+
 
 
 connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`server is running at PORT : ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MONGO db connection failed !!!", err);
+  });
 
 
 /*
-
-
-
-
 import express from "express";
 const app = express()
 (async () => {
@@ -25,13 +28,10 @@ const app = express()
       console.log("ERROR", error);
       throw error;
     });
-
     app.listen(process.env.PORT, () => {
         console.log(`APP is listinig on Port $
             {process.env.PORT}`);
     })
-
-
   } catch (error) {
     console.error("ERROR: ", error);
     throw err;
@@ -40,8 +40,3 @@ const app = express()
 
 
 */
-
-
-
-
-
