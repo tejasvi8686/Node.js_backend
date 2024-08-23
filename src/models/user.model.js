d7a01db7d30f3bafb7c7d5b3109e06915a47e704
tejavi8686 <tejasviraj8686@gmail.com>
2024-08-24 00:00:20 +0530

    added

---
+++ src/models/user.model.js
@@ -0,0 +1,34 @@
+import { type } from "express/lib/response";
+import mongoose, {Schema} from "mongoose";
+
+const userSchema = new Schema({
+  
+    username : {
+        type : String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true,
+        index: true
+    },
+    email : {
+        type : String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true
+    },
+    fullname : {
+        type : String,
+        required: true,
+        trim: true,
+        index: true
+    },
+    avatar : {
+        
+    }
+})
+
+
+
+export const User = mongoose.model("User", userSchema)
