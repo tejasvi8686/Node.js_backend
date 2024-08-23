1b7da58b4005d0df9506e0e309490424bbc7f5fb
tejavi8686 <tejasviraj8686@gmail.com>
2024-08-23 19:58:49 +0530

    node setup

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,19 +1,16 @@
 const asyncHandler = (requestHandler) => {
-     
-    
+  (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+  };
+};
 
-}
-
-
-export {asyncHandler}
-
-
+export { asyncHandler };
 
 // const asyncHandler = (fn) => async (req, res, next) => {
 
 //     try{
 
-//         await fn(req, res, next) 
+//         await fn(req, res, next)
 
 //     } catch(error) {
 //         res.status(err.code || 500 ).json({
