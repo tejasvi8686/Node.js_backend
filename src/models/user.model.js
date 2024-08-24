c59a0f582873c7d910cf2a3b1c9c12f8068e7091
tejavi8686 <tejasviraj8686@gmail.com>
2024-08-24 20:34:47 +0530

    backend added

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,34 +1,55 @@
 import { type } from "express/lib/response";
-import mongoose, {Schema} from "mongoose";
+import mongoose, { Schema } from "mongoose";
 
-const userSchema = new Schema({
-  
-    username : {
-        type : String,
-        required: true,
-        unique: true,
-        lowercase: true,
-        trim: true,
-        index: true
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      index: true,
     },
-    email : {
-        type : String,
-        required: true,
-        unique: true,
-        lowercase: true,
-        trim: true
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
     },
-    fullname : {
-        type : String,
-        required: true,
-        trim: true,
-        index: true
+    fullname: {
+      type: String,
+      required: true,
+      trim: true,
+      index: true,
     },
-    avatar : {
-        
-    }
-})
+    avatar: {
+      type: String, // cloudinary url
+      required: true,
+    },
 
+    coverImage: {
+      type: String,
+    },
 
+    watchHistory: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "video",
+      },
+    ],
 
-export const User = mongoose.model("User", userSchema)
+    password: {
+      type: String,
+      required: [true, "password is required"],
+    },
+
+    refreshToken: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+export const User = mongoose.model("User", userSchema);
