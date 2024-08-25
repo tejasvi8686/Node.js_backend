478abbb035d2e11cdad142287a2c3c39fb4d9236
tejavi8686 <tejasviraj8686@gmail.com>
2024-08-25 08:56:48 +0530

    user model

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,4 +1,5 @@
-import { type } from "express/lib/response";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 import mongoose, { Schema } from "mongoose";
 
 const userSchema = new Schema(
@@ -51,5 +52,16 @@ const userSchema = new Schema(
   },
   { timestamps: true }
 );
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+
+  this.password = bcrypt.hash(this.password, 10);
+  next();
+});
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+  await bcrypt.compare(password, this.password);
+};
 
 export const User = mongoose.model("User", userSchema);
--- src/models/video.model.js
+++ src/models/video.model.js
@@ -1,5 +1,6 @@
 import { type } from "express/lib/response";
 import mongoose, { Schema } from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
 
 const videoSchema = new Schema(
   {
@@ -44,5 +45,7 @@ const videoSchema = new Schema(
     timestamps: true,
   }
 );
+
+videoSchema.plugin(mongooseAggregatePaginate)
 
 export const Video = mongoose.model("Video", videoSchema);
